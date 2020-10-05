aeb99c74a090312bc25d3f743554aaf90614097e
liuyib <1656081615@qq.com>
2020-10-05 13:38:57 +0800

    chore: add webpack configurations

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  entry: {
+    main: "./src/index.js",
+  },
+  mode: "development" /* process.env.NODE_ENV */,
+  optimization: {
+    minimize: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [
+              [
+                "@babel/plugin-transform-react-jsx",
+                {
+                  pragma: "createElement",
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+};
