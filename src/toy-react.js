cc9cec3e78c18809a0464c708ded64eb42c8eabe
liuyib <1656081615@qq.com>
2020-10-06 23:14:04 +0800

    fix: miss child nodes during rerender

--- src/toy-react.js
+++ src/toy-react.js
@@ -62,8 +62,15 @@ export class Component {
   }
 
   rerender() {
-    this._range.deleteContents();
-    this[RENDER_TO_DOM](this._range);
+    const oldRange = this._range;
+    const range = document.createRange();
+
+    range.setStart(oldRange.startContainer, oldRange.startOffset);
+    range.setEnd(oldRange.startContainer, oldRange.startOffset);
+    this[RENDER_TO_DOM](range);
+
+    oldRange.setStart(range.endContainer, range.endOffset);
+    oldRange.deleteContents();
   }
 
   setState(newState) {
